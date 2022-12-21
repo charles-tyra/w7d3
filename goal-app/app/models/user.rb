f93c4c68af091ee5b7d317fc76eb6ba061869d38
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-21 17:28:45 -0500

    more user specs, struggling with checking is_password? method with arguments

--- goal-app/app/models/user.rb
+++ goal-app/app/models/user.rb
@@ -1,3 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
-   
+   validates :username, :session_token, uniqueness: true, presence: true
+   validates :password_digest, presence: true
+   validates :password, length: { minimum: 6 }, allow_nil: true
+
+   after_initialize :ensure_session_token
+
+   attr_reader :password
+   def password=(password)
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+   end
+
+   def self.find_by_credentials(username, password)
+      user = self.find_by(username: username)
+      if user && user.is_password?(password)
+         user
+      else
+         nil
+      end
+   end
+
+   def is_password?(password)
+      pass_obj = BCrypt::Password.new(self.password_digest)
+      pass_obj.is_password?(password)
+   end
+
+   private
+   def ensure_session_token
+      self.session_token ||= SecureRandom::urlsafe_base64
+   end
 end
+
+
+# figvapebr
--- goal-app/spec/models/user_spec.rb
+++ goal-app/spec/models/user_spec.rb
@@ -1,8 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_uniqueness_of(:username)}
-  it { should validate_uniqueness_of(:session_token)}
+  describe 'uniqueness' do
+    subject(:tobie) {
+      User.create(
+        username: 'tobie',
+        password: 'woofwoof'
+        )
+    }
+    
+    it { should validate_uniqueness_of(:username)}
+    it { should validate_uniqueness_of(:session_token)}
+  end
 
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
@@ -10,10 +30,15 @@ RSpec.describe User, type: :model do
 
   it { should validate_length_of(:password).is_at_least(6) }
 
-  context "has User::find_by_credentials" do 
-    before :each do
-      let!(:user) { User.create(username: 'tobiethedog', password: 'woofwoof')}                #'!' ?
-    end
+  it { should allow_value(nil).for(:password) }
+
+  context "has ::find_by_credentials" do 
+    subject(:tobie) {
+       User.create(
+        username: 'tobie',
+        password: 'woofwoof'
+        )
+    }
 
     # it "it should accept a username and password argument" do
       
@@ -21,7 +46,7 @@ RSpec.describe User, type: :model do
     
     context "valid user" do
       it "should return the user" do
-        expect(User.find_by_credentials(user.username, user.password)).to eq(user)
+        expect(User.find_by_credentials(tobie.username, tobie.password)).to eq(tobie)
       end
     end
 
@@ -31,9 +56,46 @@ RSpec.describe User, type: :model do
       end
     end
 
-    it "should use #is_password? helper method" do
-      expect(user).to_recieve(:is_password?).with('woofwoof')
-    end
+    # cate = User.create(username: 'cat', password:'sixcharacters')
+    # it "should use #is_password? helper method" do
+      # expect(BCrypt::Password.new(tobie.password_digest)).to receive(:is_password?).with(tobie.password).and_return(true)
+      # expect(User.find_by(username: tobie.username)).to receive(:is_password?).with(tobie.password)
+      # User.find_by_credentials(tobie.username, tobie.password)
+      # User.find_by_credentials(cat.use)
+      # expect_any_instance_of(User).to receive(:is_password?).with('woofwoof').and_return(true)
+
+    # end
+
+
     
+  end
+  describe 'is_password?' do
+    let!(:user) { create(:user) }                     #using let w bang creates the object immediately after the line and cache the object immediately, rather then lazy load
+
+    context 'with a valid password' do
+      it 'should return true' do
+        expect(user.is_password?('password')).to be true
+      end
+    end
+
+    context 'with an invalid password' do
+      it 'should return false' do
+        expect(user.is_password?('bananaman')).to be false
+      end
+    end
+  end
+
+  describe 'password hashing' do
+    it 'does not save passwords to the database' do
+      FactoryBot.create(:harry_potter)                 #or just FactoryBot.create(:user, username: 'Harry Potter')
+
+      user = User.find_by(username: 'Harry Potter')
+      expect(user.password).not_to eq('password')
+    end
+
+    it 'hashes password using BCrypt' do
+      expect(BCrypt::Password).to recieve(:create).with('abcdef')
+      FactoryBot.build(:user, password: 'abcdef')
+    end
   end
 end
